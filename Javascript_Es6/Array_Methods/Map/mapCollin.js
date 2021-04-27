72c8644b011ac57ae8b734256fe33aabe3ad2743
Collin Lanier <collinlanier12@gmail.com>
2021-04-26 23:42:38 -0400

    Collins map exercise beginning

---
+++ Javascript_Es6/Array_Methods/Map/mapCollin.js
@@ -0,0 +1,34 @@
+const dc_heroes = [
+    {
+        hero_name: "Batman",
+        hero_health: 75
+    },
+    {
+        hero_name: "Nightwing",
+        hero_health: 20
+    },
+    {
+        hero_name: "Red Hood",
+        hero_health: 18
+    },
+    {
+        hero_name: "Robin",
+        hero_health: 65
+    },
+    {
+        hero_name: "Cat Woman",
+        hero_health: 100
+    },
+];
+
+
+const removeHero = (health) => {
+    if (health < 25) {
+        health === 0;
+    }
+    return health;
+};
+
+
+console.log(newList = dc_heroes.map(x => removeHero(x.hero_health)));
+console.log(newList);
