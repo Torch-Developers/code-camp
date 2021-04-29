539aad1a6a9db87e18fe64c5b901fd364480857d
Collin Lanier <collinlanier12@gmail.com>
2021-04-29 00:08:39 -0400

    axios example complete with async funtion and file write

--- Api Calls/Axios/axiosCollin.js
+++ Api Calls/Axios/axiosCollin.js
@@ -1,8 +1,19 @@
 /* Using Fetch to make GET call */
 
 const { default: axios } = require("axios");
+const fs = require("fs");
 
-axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(response => {
-        console.log(response.data);
-    })
+const writeUsers = async () => {
+    try {
+
+        await axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(response => {
+                console.log(response.data);
+                fs.writeFile("users-axios.json", JSON.stringify(response.data, null, 2), err => {
+                    if (err) throw err;
+                });
+            })
+    } catch (err) { (console.error(err)) };
+}
+
+writeUsers();
