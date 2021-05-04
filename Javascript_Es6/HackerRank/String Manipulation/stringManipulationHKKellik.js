1a9a1f04175629439a9e4a257069973bc3e9dcef
The Glass Corner <ksimons@aggies.ncat.edu>
2021-05-03 23:29:59 -0400

    Hackerank string manipulation anagrams

---
+++ Javascript_Es6/HackerRank/String Manipulation/stringManipulationHKKellik.js
@@ -0,0 +1,14 @@
+
+/* Given two strings, a & b, that may or may not be the same length, 
+determine the number of character deletions required to make a and b anagrams.
+Any characters can be deleted from either strings.
+
+ex: if one string contains the letters of the others.
+
+a= bce, b = dbc | anagram = a(bc), b(bc) delete e and d
+*/
+
+
+
+const a = 'cde'
+const b = 'dcf
