e6ddd0d3ff68022590e967db6c01d5728e04af3c
Collin Lanier <collinlanier12@gmail.com>
2021-05-04 22:47:01 -0400

    added palindrome problem

---
+++ Javascript_Es6/HackerRank/Palindrome_Index/palindromeIndexCollin.js
@@ -0,0 +1,15 @@
+/* Given a string of lowercase letters in the range ascii[a-z],
+determine the index of a character that can be removed to make the string a palindrome.
+Input Format:
+--------------
+The first line contains an integer q, the number of queries.
+Each of the next q lines contains a query string s.
+ * Complete the 'palindromeIndex' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+*/
+
+const palindromeIndex = (s) => {
+
+}
